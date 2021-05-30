2722293bf002cea3e645b180663870e6d27448da
Sihan Xie <sihan.xie22@gmail.com>
2021-05-30 16:17:18 +0200

    require include.php file in the ficheclient.php

--- Devoir 3/ficheclient.php
+++ Devoir 3/ficheclient.php
@@ -1,7 +1,5 @@
 <?php
-ini_set( 'session.cookie_httponly', 1 );
-error_reporting(0);
-ini_set("display_errors", 0);
+require_once('include.php');
 session_start();
 $url_redirect = "index.php";
 if(!isset($_SESSION["connected_user"])){
