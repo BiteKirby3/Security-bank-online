c912d8ce605846feeeb3538c5503bb02f426a510
Sihan Xie <sihan.xie22@gmail.com>
2021-05-31 16:29:26 +0200

    fix

--- Devoir 3/config/config.php
+++ Devoir 3/config/config.php
@@ -1,6 +1,6 @@
 <?php
   define('DB_HOST','127.0.0.1:3306');
-  define('DB_USER','guan');
-  define('DB_PASSWD','0724');
+  define('DB_USER','root');
+  define('DB_PASSWD','root');
   define('DB_NAME','devoir-sr03');
 ?>
